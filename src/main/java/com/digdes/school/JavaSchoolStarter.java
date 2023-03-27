9fc68c3bf79931e2ef9afe591ac9799c982339bb
Nikita1306 <skywalker1306@gmail.com>
2023-03-27 21:08:30 +0300

    Added basic UPDATE operation handling with one condition

--- src/main/java/com/digdes/school/JavaSchoolStarter.java
+++ src/main/java/com/digdes/school/JavaSchoolStarter.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.Map;
 
 public class JavaSchoolStarter {
-
+    private List<Map<String, Object>> data = new ArrayList<>();
     //Column names
     private static final String columnId = "'id'";
     private static final String columnLastName = "'lastname'";
@@ -33,7 +33,7 @@ public class JavaSchoolStarter {
                 return insertOperation(request.substring(request.toLowerCase().indexOf("values") + 6));
             }
             case update: {
-                break;
+                return updateOperation(request.substring(request.toLowerCase().indexOf("values") + 6));
             }
             case delete: {
                 break;
@@ -50,8 +50,8 @@ public class JavaSchoolStarter {
         List<Map<String, Object>> inputRow = new ArrayList<>();
         System.out.println(request);
         Map<String,Object> row = new HashMap<>();
-        String[] data = request.split(",");
-        for (String i : data) {
+        String[] dataSplitted = request.split(",");
+        for (String i : dataSplitted) {
             i = i.trim();
             String[] pair = i.split("=");
             //TODO: Поменять вставку ключей с констант на обычные (не 'lastname', а lastName и также в других)
@@ -77,10 +77,68 @@ public class JavaSchoolStarter {
                     break;
                 }
             }
-            System.out.println(i);
         }
-        System.out.println(row);
         inputRow.add(row);
+        data.add(row);
         return inputRow;
+    }
+
+    private List<Map<String,Object>> updateOperation(String request) {
+        List<Map<String, Object>> inputRow = new ArrayList<>();
+        System.out.println(request);
+        String condition = request.substring(request.toLowerCase().indexOf("where") + 6);
+        System.out.println(condition);
+        Map<String,Object> row = new HashMap<>();
+        String[] updateRows = request.substring(0, request.toLowerCase().indexOf("where")).split(",");
+        for (String i : updateRows) {
+            i = i.trim();
+            String[] pair = i.split("=");
+            System.out.println(i);
+            //TODO: Поменять вставку ключей с констант на обычные (не 'lastname', а lastName и также в других)
+            switch(pair[0].trim().toLowerCase()) {
+                case columnLastName: {
+                    row.put(columnLastName.replace("'", ""), pair[1].trim().replace("'", ""));
+                    break;
+                }
+                case columnId: {
+                    row.put(columnId.replace("'", ""),Long.valueOf(pair[1].trim()));
+                    break;
+                }
+                case columnAge: {
+                    row.put(columnAge.replace("'", ""),Long.valueOf(pair[1].trim()));
+                    break;
+                }
+                case columnCost: {
+                    row.put(columnCost.replace("'", ""),Double.valueOf(pair[1].trim()));
+                    break;
+                }
+                case columnActive: {
+                    row.put(columnActive.replace("'", ""),Boolean.valueOf(pair[1].trim()));
+                    break;
+                }
+            }
+        }
+        for (int i = 0; i < data.size(); i++) {
+            Map<String, Object> tempValue = data.get(i);
+            if (row.containsKey("active") && row.get("active") != data.get(i).get("active")) {
+                tempValue.put("active", row.get("active"));
+            }
+            if (row.containsKey("cost") && row.get("cost") != data.get(i).get("cost")) {
+                tempValue.put("cost", row.get("cost"));
+            }
+            if (row.containsKey("id") && row.get("id") != data.get(i).get("id")) {
+                tempValue.put("id", row.get("id"));
+            }
+            if (row.containsKey("age") && row.get("age") != data.get(i).get("age")) {
+                tempValue.put("age", row.get("age"));
+            }
+            if (row.containsKey("lastname") && row.get("lastname") != data.get(i).get("lastname")) {
+                tempValue.put("lastname", row.get("lastname"));
+            }
+            data.set(i, tempValue);
+        }
+//        inputRow.add(row);
+//        return inputRow;
+        return data;
     }
 }
